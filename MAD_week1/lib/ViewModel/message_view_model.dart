365fb26ba1d1da437c453ee0c4611ee6cf3f08d4
박태건 <geon@bagtaegeon-ui-MacBookAir.local>
2024-12-31 16:39:23 +0900

    add logo

--- MAD_week1/lib/ViewModel/message_view_model.dart
+++ MAD_week1/lib/ViewModel/message_view_model.dart
@@ -3,42 +3,34 @@ import 'package:flutter/foundation.dart';
 import '../Model/message.dart'; // Comment 모델 import
 import 'package:google_generative_ai/google_generative_ai.dart'; // Google Generative AI 라이브러리 import
 
-class CommentViewModel extends ChangeNotifier {
-  final GenerativeModel model; // GenerativeModel 주입
-  List<Comment> comments = [
-    Comment(content: "안녕하세요", author: "홍길동", timestamp: DateTime.now()), // 초기 댓글
-  ];
-  TextEditingController commentController = TextEditingController();
+class SpamFilterViewModel extends ChangeNotifier {
+  final GenerativeModel model;
+  List<String> spamMessages = [];
+  List<String> nonSpamMessages = [];
 
-  // 생성자에서 GenerativeModel 주입받기
-  CommentViewModel(this.model);
+  SpamFilterViewModel(this.model);
 
-  // 댓글 추가 메서드
-  Future<bool> addComment(String content, String author) async {
-    if (content.isNotEmpty) {
-      Comment newComment = Comment(
-        content: content,
-        timestamp: DateTime.now(),
-        author: author,
-      );
+  Future<bool?> checkSpam(String message) async {
+    try {
+      final prompt =
+          'Classify the following message as spam or not spam. Answer it only in spam or not spam: "$message"';
+      final response = await model.generateContent([Content.text(prompt)]);
 
-      // Google Generative AI를 통해 스팸 여부 확인
-      await newComment.checkSpamStatusWithModel(model);
-
-      if (newComment.isSpam == false) {
-        comments.add(newComment); // 스팸이 아니면 댓글 추가
-        notifyListeners(); // UI 업데이트
-        return true; // 정상 메시지
-      } else {
-        return false; // 스팸 메시지
+      if (response.text != null) {
+        final classification = response.text!.trim().toLowerCase();
+        if (classification.contains('not spam')) {
+          nonSpamMessages.add(message);
+          notifyListeners();
+          return false;
+        } else if (classification.contains('spam')) {
+          spamMessages.add(message);
+          notifyListeners();
+          return true;
+        }
       }
+    } catch (e) {
+      print("오류 발생: $e");
     }
-    return false; // 빈 메시지는 추가하지 않음
-  }
-
-  @override
-  void dispose() {
-    commentController.dispose(); // 컨트롤러 해제
-    super.dispose();
+    return null; // 결과가 불분명한 경우
   }
 }
